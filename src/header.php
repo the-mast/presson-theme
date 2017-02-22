07c4e494e14882592ad9870f6515b5e64fe6c20c
sw00 <sett.wai@gmail.com>
2017-02-22 12:01:13 +0200

    [sett] pull in jquery

--- src/header.php
+++ src/header.php
@@ -14,7 +14,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <?php wp_head(); ?>
 </head>
 
