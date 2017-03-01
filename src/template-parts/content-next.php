a05393a2c795e12bd568474f602ddfff84e6edd1
Felix Moalusi <felixm@thoughtworks.com>
2017-03-01 15:46:25 +0200

    [felix] added styling for next-article without a thumbnail

--- src/template-parts/content-next.php
+++ src/template-parts/content-next.php
@@ -12,25 +12,33 @@ if (!empty( $next_post )): ?>
             $post = $next_post;
             $next_post_category = get_the_category();
             $category_link = get_category_link( $next_post_category[0]->term_id );
-            the_post_thumbnail();
+            //the_post_thumbnail();
          ?>
+         <div class="" href="#">
+             <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+        </div>
+        <?php
+        if ( !empty (get_the_post_thumbnail()) ) : ?>
         <div class="next-image-box">
-            <?php
-            if ( !empty (get_the_post_thumbnail()) ) : ?>
             <div class="next-title-overlay">
                 <a href="<?php echo esc_html( $category_link ); ?>"><?php echo esc_html( $next_post_category[0]->name ); ?></a>
                 <h3 class="next-article-titles">
                     <a href="<?php echo get_permalink( $next_post->class ); ?>"><?php echo get_the_title( $next_post->class ); ?></a>
                 </h3>
             </div>
-            <?php else: ?>
+        </div>
+        <?php else: ?>
+        <div class="next-article-box">
             <div class="no-thumbnail">
+                <div class="next-article-category">
+                    <a href="<?php echo esc_html( $category_link ); ?>"><?php echo esc_html( $next_post_category[0]->name ); ?></a>
+                </div>
                 <h3>
                     <a href="<?php echo get_permalink( $next_post->class ); ?>"><?php echo get_the_title( $next_post->class ); ?></a>
                 </h3>
             </div>
-            <?php endif; ?>
         </div>
+        <?php endif; ?>
     </div>
 </div>
 <?php endif; ?>
