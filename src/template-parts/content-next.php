d464be3402a8fabe593595e9258a272f3dc78a67
Felix Moalusi <felixm@thoughtworks.com>
2017-03-01 12:15:54 +0200

    [felix] added the category name to the image box

--- src/template-parts/content-next.php
+++ src/template-parts/content-next.php
@@ -10,12 +10,15 @@ if (!empty( $next_post )): ?>
     <div class="next-image-container">
         <?php
             $post = $next_post;
+            $next_post_category = get_the_category();
+            $category_link = get_category_link( $next_post_category[0]->term_id );
             the_post_thumbnail();
          ?>
         <div class="next-image-box">
             <?php
-            if ( has_post_thumbnail() ) : ?>
+            if ( !empty (get_the_post_thumbnail()) ) : ?>
             <div class="next-title-overlay">
+                <a href="<?php echo esc_html( $category_link ); ?>"><?php echo esc_html( $next_post_category[0]->name ); ?></a>
                 <h3 class="next-article-titles">
                     <a href="<?php echo get_permalink( $next_post->class ); ?>"><?php echo get_the_title( $next_post->class ); ?></a>
                 </h3>
