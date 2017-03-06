44a2a1175f5c80cbe59c17914293809bae737ba2
Felix Moalusi <felixm@thoughtworks.com>
2017-03-06 16:11:30 +0200

    [felix] clickable read next article image

--- src/template-parts/content-article-head.php
+++ src/template-parts/content-article-head.php
@@ -3,11 +3,13 @@ $post_category = get_the_category();
 $category_link = get_category_link( $post_category[0]->term_id );
 if ( !empty (get_the_post_thumbnail()) ) : ?>
 <div class="image-container">
-	<?php the_post_thumbnail(); ?>
 	<div class="image-box">
-		<div class="article-category">
+		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail');?></a>
+		
+		<div class="category-overlay">
             <a href="<?php echo esc_html( $category_link ); ?>"><?php echo esc_html( $post_category[0]->name ); ?></a>
         </div>
+
 		<div class="title-overlay">
 			<?php
 				the_title( '<h3 class="next-article-category"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
