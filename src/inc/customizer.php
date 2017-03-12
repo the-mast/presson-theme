a2a82060647a5ed96e398bb50e4d3c4899643201
Tsatsi <tsatsi.mahase@gmail.com>
2017-03-12 10:34:06 +0200

    adding customiser

--- src/inc/customizer.php
+++ src/inc/customizer.php
@@ -47,6 +47,42 @@ function press_on_customize_register( $wp_customize ) {
         )
     ));
 
+    $wp_customize->add_setting(
+        'accent_color',
+        array(
+            'default' => '#E4DBCC'
+        ));
+    
+    $wp_customize->add_control(
+    new WP_Customize_Color_Control(
+        $wp_customize,
+        'accent_color',
+        array(
+            'label'      => __( 'Accent color', 'presson' ),
+            'section'    => 'colors',
+            'settings'   => 'accent_color'
+        )
+    ));
+
+    $wp_customize->add_setting(
+        'highlight',
+        array(
+            'default' => '#E34357'
+    ));
+    
+    $wp_customize->add_control(
+    new WP_Customize_Color_Control(
+        $wp_customize,
+        'highlight',
+        array(
+            'label'      => __( 'highlight color', 'presson' ),
+            'section'    => 'colors',
+            'settings'   => 'highlight'
+        )
+    ));
+
+
+
 
 }
 add_action( 'customize_register', 'press_on_customize_register' );
@@ -58,3 +94,47 @@ function press_on_customize_preview_js() {
 	wp_enqueue_script( 'press_on_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'press_on_customize_preview_js' );
+
+
+function presson_customizer_css() {
+    ?>
+    <style type="text/css">
+        #masthead { 
+             background-color: <?php echo get_theme_mod( 'primary_color' ); ?>; 
+        }
+        a{
+             color:<?php echo get_theme_mod( 'highlight' ); ?>;
+        }
+        a :hover{
+             color:<?php echo get_theme_mod( 'secondary_color' ); ?>;
+        }
+        a :focus {
+             color:<?php echo get_theme_mod( 'secondary_color' ); ?>;
+        }
+	    a:active {
+             color:<?php echo get_theme_mod( 'secondary_color' ); ?>;
+        }
+        .related-story .related-story-title span {
+            color: <?php echo get_theme_mod( 'highlight' ); ?>;
+        }
+        .article .category a, .article .image-container .category-overlay a, .article .text-article-container .article-category a, .featured-article .category a, .featured-article .image-container .category-overlay a, .featured-article .text-article-container .article-category a {
+            color: <?php echo get_theme_mod( 'highlight' ); ?>;
+        }
+        .archive .archive-header h1 {
+            color: <?php echo get_theme_mod( 'primary_color' ); ?>;
+        }
+        .archive .archive-header {
+         border-color: <?php echo get_theme_mod( 'primary_color' ); ?>;
+        }
+        #mastfoot{
+                background-color: <?php echo get_theme_mod( 'primary_color' ); ?>; 
+        }
+        .related-story {
+            border-color: <?php echo get_theme_mod( 'accent_color' ); ?>; 
+        }
+
+    </style>
+    <?php
+}
+add_action( 'wp_head', 'presson_customizer_css' );
+
