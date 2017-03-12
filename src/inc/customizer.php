6343f73225ae333067e0243ffba394597ef7f956
Tsatsi <tsatsi.mahase@gmail.com>
2017-03-12 08:08:23 +0200

    adding primary color and secondary to customizer

--- src/inc/customizer.php
+++ src/inc/customizer.php
@@ -14,6 +14,44 @@ function press_on_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	
+    $wp_customize->remove_control('header_textcolor');
+
+	$wp_customize->add_setting(
+    'primary_color',
+    array(
+        'default'     => '#500850'
+    ));
+
+	$wp_customize->add_control(
+    new WP_Customize_Color_Control(
+        $wp_customize,
+        'primary_color',
+        array(
+            'label'      => __( 'Primary color', 'presson' ),
+            'section'    => 'colors',
+            'settings'   => 'primary_color'
+        )
+    ));
+
+	$wp_customize->add_setting(
+    'secondary_color',
+    array(
+        'default'     => '#192755'
+    ));
+
+	$wp_customize->add_control(
+    new WP_Customize_Color_Control(
+        $wp_customize,
+        'secondary_color',
+        array(
+            'label'      => __( 'Secondary color', 'presson' ),
+            'section'    => 'colors',
+            'settings'   => 'secondary_color'
+        )
+    ));
+
+
 }
 add_action( 'customize_register', 'press_on_customize_register' );
 
