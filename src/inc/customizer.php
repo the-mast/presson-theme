83aad532d89ca3ee98749c481d8f407336e50ba8
Farzana Rassul <frassul@thoughtworks.com>
2017-03-14 18:26:32 +0200

    [Farzana, Neil] correcting path to customize js

--- src/inc/customizer.php
+++ src/inc/customizer.php
@@ -144,7 +144,7 @@ add_action( 'customize_register', 'press_on_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function press_on_customize_preview_js() {
-	wp_enqueue_script( 'press_on_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'press_on_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'press_on_customize_preview_js' );
 
