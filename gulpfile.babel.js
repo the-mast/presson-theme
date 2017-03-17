42f8b468c982bb445375819899a7530b0b6a4f20
sw00 <sett.wai@gmail.com>
2017-03-17 14:20:04 +0200

    [sett] optimise the build tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -30,7 +30,6 @@ import del from 'del';
 import runSequence from 'run-sequence';
 import browserSync from 'browser-sync';
 import moment from 'moment';
-import swPrecache from 'sw-precache';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import { output as pagespeed } from 'psi';
 import pkg from './package.json';
@@ -49,7 +48,7 @@ var PATHS = {
 
 // Lint JavaScript
 gulp.task('lint', () =>
-    gulp.src(['app/scripts/**/*.js', '!node_modules/**'])
+    gulp.src(['src/js/**/*.js', '!node_modules/**'])
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
@@ -57,7 +56,7 @@ gulp.task('lint', () =>
 
 // Optimize images
 gulp.task('images', () =>
-    gulp.src('app/images/**/*')
+    gulp.src('src/images/**/*')
     .pipe($.cache($.imagemin({
         progressive: true,
         interlaced: true
@@ -66,53 +65,7 @@ gulp.task('images', () =>
     .pipe($.size({ title: 'images' }))
 );
 
-// Copy all files at the root level (app)
-gulp.task('copy', () =>
-    gulp.src([
-        'app/*',
-        '!app/*.html',
-        'node_modules/apache-server-configs/dist/.htaccess'
-    ], {
-        dot: true
-    }).pipe(gulp.dest('dist'))
-    .pipe($.size({ title: 'copy' }))
-);
-
-// Compile and automatically prefix stylesheets
 gulp.task('styles', () => {
-    const AUTOPREFIXER_BROWSERS = [
-        'ie >= 10',
-        'ie_mob >= 10',
-        'ff >= 30',
-        'chrome >= 34',
-        'safari >= 7',
-        'opera >= 23',
-        'ios >= 7',
-        'android >= 4.4',
-        'bb >= 10'
-    ];
-
-    var neat = require('node-neat').includePaths;
-
-    // For best performance, don't add Sass partials to `gulp.src`
-    return gulp.src('app/src/**/style.scss')
-        .pipe($.newer('.tmp/styles'))
-        .pipe($.sourcemaps.init())
-        .pipe($.sass({
-            precision: 10,
-            includePaths: ['app/src/**/*.scss'].concat(neat)
-        }).on('error', $.sass.logError))
-        .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-        .pipe(gulp.dest('.tmp/styles'))
-        // Concatenate and minify styles
-        .pipe($.if('*.css', $.cssnano({ zindex: false })))
-        .pipe($.size({ title: 'styles' }))
-        .pipe($.sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/'))
-        .pipe(gulp.dest('.tmp/styles'));
-});
-
-gulp.task('styles:underscore', () => {
     const AUTOPREFIXER_BROWSERS = [
         'ie >= 10',
         'ie_mob >= 10',
@@ -138,7 +91,7 @@ gulp.task('styles:underscore', () => {
         .pipe($.if('*.css', $.cssnano({ zindex: false })))
         .pipe($.size({ title: 'styles' }))
         .pipe($.sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('src/'));
 });
 
 // Concatenate and minify JavaScript. Optionally transpiles ES2015 code to ES5.
@@ -169,87 +122,22 @@ gulp.task('scripts', () =>
     .pipe(gulp.dest('.tmp/scripts'))
 );
 
-// Scan your HTML for assets & optimize them
-gulp.task('html', () => {
-    return gulp.src('app/**/*.html')
-        .pipe($.useref({
-            searchPath: '{.tmp,app}',
-            noAssets: true
-        }))
-
-    // Minify any HTML
-    .pipe($.if('*.html', $.htmlmin({
-            removeComments: true,
-            collapseWhitespace: true,
-            collapseBooleanAttributes: true,
-            removeAttributeQuotes: true,
-            removeRedundantAttributes: true,
-            removeEmptyAttributes: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            removeOptionalTags: true
-        })))
-        // Output files
-        .pipe($.if('*.html', $.size({ title: 'html', showFiles: true })))
-        .pipe(gulp.dest('dist'));
-});
-
 // Clean output directory
 gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], { dot: true }));
 
-// Watch files for changes & reload
-gulp.task('serve', ['scripts', 'styles'], () => {
-    browserSync({
-        notify: false,
-        // Customize the Browsersync console logging prefix
-        logPrefix: 'WSK',
-        // Allow scroll syncing across breakpoints
-        scrollElementMapping: ['main', '.mdl-layout'],
-        // Run as an https by uncommenting 'https: true'
-        // Note: this uses an unsigned certificate which on first access
-        //       will present a certificate warning in the browser.
-        // https: true,
-        server: ['.tmp', 'app'],
-        port: 3000
-    });
-
-    gulp.watch(['app/**/*.html'], reload);
-    gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-    gulp.watch(['app/src/**/*.scss'], ['styles', reload]);
-    gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
-    gulp.watch(['app/images/**/*'], reload);
+gulp.task('copy:style', ['styles'], () => {
+    return gulp.src(['src/style.css', 'src/style.css.map']).pipe(gulp.dest('dist/'));
 });
-
-// Build and serve the output from the dist build
-gulp.task('serve:dist', ['default'], () =>
-    browserSync({
-        notify: false,
-        logPrefix: 'WSK',
-        // Allow scroll syncing across breakpoints
-        scrollElementMapping: ['main', '.mdl-layout'],
-        // Run as an https by uncommenting 'https: true'
-        // Note: this uses an unsigned certificate which on first access
-        //       will present a certificate warning in the browser.
-        // https: true,
-        server: 'dist',
-        port: 3001
-    })
-);
-
-gulp.task('copy_images', () => {
+gulp.task('copy:images', ['images'], () => {
     return gulp.src('src/images/**/*').pipe(gulp.dest('dist/assets/images/'));
 });
 
-gulp.task('copy_assets', () => {
-    return gulp.src('src/js/**/*.js').pipe(gulp.dest('dist/assets/js/'));
-});
-
-gulp.task('copy_phpfiles', () => {
+gulp.task('copy:php', () => {
     return gulp.src('src/**/*.php').pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', ['clean', 'styles:underscore'], cb => {
-    runSequence(['scripts', 'copy_images', 'copy_assets', 'copy_phpfiles'], 'copy-sw-scripts', 'generate-service-worker', cb);
+gulp.task('build', ['clean', 'styles'], cb => {
+    runSequence(['copy:style', 'copy:images', 'scripts', 'copy:php'], cb);
 });
 
 // PHP Code Sniffer task
@@ -278,31 +166,30 @@ gulp.task('phpcbf', function() {
 
 // Package task
 gulp.task('package', ['build'], function() {
-    var title = 'news-day-theme' + '_' + moment().format("YYYY-MM-DD_HH-mm") + '.zip';
+    var title = 'presson-theme' + '_' + moment().format("YYYY-MM-DD_HH-mm") + '.zip';
 
     return gulp.src('dist/**/*')
         .pipe($.zip(title))
         .pipe(gulp.dest('packaged'));
 });
 
-gulp.task('serve:wordpress', ['styles:underscore'], function() {
+gulp.task('serve:wordpress', ['build'], function() {
     browserSync({
         notify: false,
         // Customize the Browsersync console logging prefix
         proxy: 'http://localhost:8080'
     });
 
-    gulp.watch(['src/sass/**/*.scss'], ['clean', 'styles:underscore', 'build', reload]);
-    gulp.watch(['src/**/*.php'], ['clean', 'build', reload]);
-    gulp.watch(['src/images/**/*'], ['clean', 'build', reload]);
-    gulp.watch(['src/js/**/*'], ['clean', 'build', reload]);
+    gulp.watch(['src/sass/**/*.scss'], ['styles', reload]);
+    gulp.watch(['src/**/*.php'], ['copy:php', reload]);
+    gulp.watch(['src/images/**/*'], ['copy:images', reload]);
+    gulp.watch(['src/js/**/*'], ['lint', 'scripts', 'copy:scripts', reload]);
 });
 
 // Build production files, the default task
 gulp.task('default', ['clean'], cb =>
     runSequence(
-        'styles', ['lint', 'html', 'scripts', 'images', 'copy'],
-        'generate-service-worker',
+        'lint', 'styles', ['build'],
         cb
     )
 );
@@ -310,50 +197,13 @@ gulp.task('default', ['clean'], cb =>
 // Run PageSpeed Insights
 gulp.task('pagespeed', cb =>
     // Update the below URL to the public URL of your site
-    pagespeed('http://staging.themastonline.com', {
+    pagespeed('', {
         strategy: 'mobile'
             // By default we use the PageSpeed Insights free (no API key) tier.
             // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
             // key: 'YOUR_API_KEY'
     }, cb)
 );
-
-// Copy over the scripts that are used in importScripts as part of the generate-service-worker task.
-gulp.task('copy-sw-scripts', () => {
-    return gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'app/scripts/sw/runtime-caching.js'])
-        .pipe(gulp.dest('dist/assets/js/sw'));
-});
-
-// See http://www.html5rocks.com/en/tutorials/service-worker/introduction/ for
-// an in-depth explanation of what service workers are and why you should care.
-// Generate a service worker file that will provide offline functionality for
-// local resources. This should only be done for the 'dist' directory, to allow
-// live reload to work as expected when serving from the 'app' directory.
-gulp.task('generate-service-worker', ['copy-sw-scripts'], () => {
-    const rootDir = 'dist';
-    const filepath = path.join(rootDir, 'assets/js/service-worker.js');
-
-    return swPrecache.write(filepath, {
-        // Used to avoid cache conflicts when serving on localhost.
-        cacheId: pkg.name || 'presson-theme',
-        // sw-toolbox.js needs to be listed first. It sets up methods used in runtime-caching.js.
-        importScripts: [
-            'assets/js/sw/sw-toolbox.js',
-            'assets/js/sw/runtime-caching.js'
-        ],
-        staticFileGlobs: [
-            // Add/remove glob patterns to match your directory setup.
-            `${rootDir}/assets/images/**/*`,
-            `${rootDir}/assets/js/**/*.js`,
-            `${rootDir}/**/*.css`,
-            `${rootDir}/*.{html,json}`
-        ],
-        // Translates a static file path to the relative URL that it's served from.
-        // This is '/' rather than path.sep because the paths returned from
-        // glob always use '/'.
-        stripPrefix: rootDir + '/'
-    });
-});
 
 // Load custom tasks from the `tasks` directory
 // Run: `npm install --save-dev require-dir` from the command-line
