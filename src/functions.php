1701e8e9de26019b9a2a72e9f639e1eeb37956dc
Felix Moalusi <felixm@thoughtworks.com>
2017-03-20 11:34:47 +0200

    [felix] disable empty search

--- src/functions.php
+++ src/functions.php
@@ -233,6 +233,16 @@ require get_template_directory() . '/inc/customizer.php';
  */
 require get_template_directory() . '/inc/jetpack.php';
 
+/**
+ * Halt the main query in the case of an empty search 
+ */
+add_filter( 'posts_search', function( $search, \WP_Query $q )
+{
+    if( ! is_admin() && empty( $search ) && $q->is_search() && $q->is_main_query() )
+        $search .=" AND 0=1 ";
+
+    return $search;
+}, 10, 2 );
 
 /**
  * Advertising
