e5913e27ee716ba6a2fd475f1b2c19c8eba6ba66
Felix Moalusi <felixm@thoughtworks.com>
2017-03-20 11:43:04 +0200

    [felix] add search term to results searchbox

--- src/searchform.php
+++ src/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" id="search-form" action="/" >
     <label>
         <span class="screen-reader-text">Search</span>
-        <input type="search" class="search-field" placeholder="Enter search …" value="" name="s">
+        <input type="search" class="search-field" placeholder="Enter search …" value="<?php if( is_search() ){ echo get_search_query(); } ?>" name="s">
     </label>
     <input type="submit" class="search-submit button" value="Go">
 </form>
