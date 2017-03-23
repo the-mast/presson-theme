119e7311a5c1600d9cc7f8aa0404e0e364aec659
Farzana Rassul <frassul@thoughtworks.com>
2017-03-23 14:23:44 +0200

    [Farzana] implementing full length titles where there is no thumbnail image.

--- src/template-parts/content-index-posts.php
+++ src/template-parts/content-index-posts.php
@@ -29,9 +29,12 @@
             setup_postdata('$post');
             array_push($excludelist, $post->ID);
             $categories = get_the_category();
+
+            if(!empty (get_the_post_thumbnail())):
             ?>
+            
             <div class="related-story">
-                <div class="related-story-title">
+                <div class="related-story-title title">
                     <?php
                     foreach ($categories as $cat):
                         if (!in_array($cat->term_id, $excludes)) :
@@ -46,7 +49,22 @@
                     <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
                 </div>
             </div>
-            <?php
+
+        <?php else: ?>
+                <div class="related-story">
+                    <div class="full-length-title title">
+                        <?php
+                            foreach ($categories as $cat): 
+                                if( !in_array($cat->term_id, $excludes)) :
+                                $category_link = get_category_link( $cat->term_id );
+                                echo '<a href="' . esc_html($category_link) . '"><span>' . esc_html( $cat->name ) . '</span> </a>';
+                                echo ' ';
+                                endif;
+                            endforeach ?>
+                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    </div>
+                </div>
+        <?php endif;
         endforeach;
         wp_reset_postdata;
 }
--- src/template-parts/content-related.php
+++ src/template-parts/content-related.php
@@ -38,24 +38,41 @@
 		<?php foreach( $postslist as $post ):
 			setup_postdata('$post');
 			$categories = get_the_category();
-			?>
-				<div class="related-story">
-					<div class="related-story-title">
-						<?php
-							foreach ($categories as $cat): 
-								if( !in_array($cat->term_id, $excludes)) :
-							 	$category_link = get_category_link( $cat->term_id );
-								echo '<a href="' . esc_html($category_link) . '"><span>' . esc_html( $cat->name ) . '</span> </a>';
-								echo ' ';
-								endif;
-							endforeach ?>
-						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+				if(!empty (get_the_post_thumbnail())):
+				?>
+					<div class="related-story">
+						<div class="related-story-title title">
+							<?php
+								foreach ($categories as $cat): 
+									if( !in_array($cat->term_id, $excludes)) :
+									$category_link = get_category_link( $cat->term_id );
+									echo '<a href="' . esc_html($category_link) . '"><span>' . esc_html( $cat->name ) . '</span> </a>';
+									echo ' ';
+									endif;
+								endforeach ?>
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						</div>
+						<div class="related-story-image">
+							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+						</div>
 					</div>
-					<div class="related-story-image">
-						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+				<?php 
+				else: ?>
+					<div class="related-story">
+						<div class="full-length-title title">
+							<?php
+								foreach ($categories as $cat): 
+									if( !in_array($cat->term_id, $excludes)) :
+									$category_link = get_category_link( $cat->term_id );
+									echo '<a href="' . esc_html($category_link) . '"><span>' . esc_html( $cat->name ) . '</span> </a>';
+									echo ' ';
+									endif;
+								endforeach ?>
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						</div>
 					</div>
-				</div>
-			<?php 
+				<?php endif;
 			endforeach;
 			wp_reset_postdata;
 		}?>
