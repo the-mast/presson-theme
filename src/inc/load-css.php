ef5df04f4d0bf41a40c86038b3d9174fe822d8e0
sw00 <sett.wai@gmail.com>
2017-03-30 12:32:00 +0200

    [sett] fix bug to render other stylesheets

--- src/inc/load-css.php
+++ src/inc/load-css.php
@@ -11,6 +11,9 @@ function load_css_async($html, $handle, $href, $media) {
 	echo '<noscript><link rel="stylesheet" href="' . $href . '"></noscript>';
 	return str_replace("rel='stylesheet'", "rel='preload' as='style' onload='this.rel=\"stylesheet\";'", $html);
       }
+      else{
+	  return $html;
+      }
     }
 }
 add_filter('style_loader_tag', 'load_css_async', 10, 4);
