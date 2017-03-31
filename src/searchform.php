ace087f0ce6bd9dff0d1b6d6b078fe7293effec1
Felix Moalusi <felixm@thoughtworks.com>
2017-03-31 13:30:11 +0200

    [felix] styling for search field

--- src/searchform.php
+++ src/searchform.php
@@ -1,6 +1,5 @@
 <form role="search" method="get" id="search-form" action="/" >
     <label>
-        <span class="screen-reader-text">Search</span>
         <input type="search" class="search-field" placeholder="Enter search …" value="<?php if( is_search() ){ echo get_search_query(); } ?>" name="s">
     </label>
     <input type="submit" class="search-submit button" value="Go">
